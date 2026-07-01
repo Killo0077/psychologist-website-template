6157eb8582582388a7489372b565284ec498d1dc
Killo007 <pablobaenav@gmail.com>
2026-07-01 20:29:46 +0100

    Prepare Cloudflare production config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://killo0077.github.io",
-  base: "/psychologist-website-template/",
+  site: "https://drvictoriahowson.com",
 });
